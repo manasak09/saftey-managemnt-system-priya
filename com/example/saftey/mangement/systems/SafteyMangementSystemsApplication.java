d08f402a97c09570a9084c8312e179e12c3bf3fd
Group6 <103427156+manasak09@users.noreply.github.com>
2022-04-18 11:10:53 +0530

    Add files via upload

---
+++ com/example/saftey/mangement/systems/SafteyMangementSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.saftey.mangement.systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafteyMangementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafteyMangementSystemsApplication.class, args);
+	}
+
+}
